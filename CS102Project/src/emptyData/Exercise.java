dc841ecd9e3cf64ed62fd1368c9ec12f41db6667
Aykhan Ahmadzada <72258739+21903609@users.noreply.github.com>
2020-12-18 11:35:44 +0300

    Create Exercise.java

---
+++ CS102Project/src/emptyData/Exercise.java
@@ -0,0 +1,12 @@
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class Exercise extends JPanel {
+	
+}
